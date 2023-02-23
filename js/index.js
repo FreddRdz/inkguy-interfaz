815c0097c298e96a88bb86736b0d6a9df9006977
Alfredo Rodriguez <fredd.073100@gmail.com>
2023-02-23 00:17:10 -0600

    menu responsive

--- js/index.js
+++ js/index.js
@@ -10,12 +10,41 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+const navBar = document.querySelector('.nav-bar');
+let changeDisplay = true;
 
 document.addEventListener('scroll', () => {
-  const navBar = document.querySelector('.nav-bar');
   if (window.scrollY > 0) {
     navBar.classList.add('scrolled');
   } else {
-    navBar.classList.remove('scrolled');
+    if (changeDisplay) {
+      navBar.classList.remove('scrolled');
+    }
+  }
+});
+
+const iconoBarras = document.querySelector('.icono-barra');
+const navbarLinks = document.querySelector('.nav-bar > ul');
+const iconoNav = document.querySelector('.icono-barra');
+
+iconoBarras.addEventListener('click', () => {
+  if (changeDisplay) {
+    navbarLinks.style.display = 'flex';
+    changeDisplay = false;
+    navBar.classList.add('scrolled');
+    navbarLinks.classList.add('appear');
+    iconoNav.classList.remove('fa-bars');
+    iconoNav.classList.add('fa-x');
+  } else {
+    navbarLinks.style.display = 'none';
+    changeDisplay = true;
+
+    if (window.scrollY === 0) {
+      navBar.classList.remove('scrolled');
+    }
+    navbarLinks.classList.remove('appear');
+
+    iconoNav.classList.add('fa-bars');
+    iconoNav.classList.remove('fa-x');
   }
 });
